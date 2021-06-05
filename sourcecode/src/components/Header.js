f4ef437fc833b2bfb3bab403a4e3557053c17151
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-05 12:50:55 +0530

    logo and search bar done

--- sourcecode/src/components/Header.js
+++ sourcecode/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import "./Header.css";
+import {Link} from "react-router-dom";
+import SearchIcon from '@material-ui/icons/Search';
 
 function navbar() {
-    return <nav className="navbar">
-             <img 
-             className="navbarLogo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
+    return (
+        <nav className="navbar">
+            {/*LOGO*/}
+            <Link to="/">
+            <img className="navbarLogo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
+            </Link>
+             {/*SEARCH BAR*/}
+                <div className="navbarSearch">
+                  <input type="text" className="navbarSearchInput" />
+                  <SearchIcon className="navbarSearchIcon" />
+                </div>
         </nav>
+    );
     
 }
 
