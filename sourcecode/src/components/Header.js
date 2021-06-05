94c7717d6b5df2cb5da3adaabeb049088c73d024
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-05 13:03:43 +0530

    navbar completed 75%

--- sourcecode/src/components/Header.js
+++ sourcecode/src/components/Header.js
@@ -10,11 +10,23 @@ function navbar() {
             <Link to="/">
             <img className="navbarLogo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
             </Link>
+
              {/*SEARCH BAR*/}
                 <div className="navbarSearch">
                   <input type="text" className="navbarSearchInput" />
                   <SearchIcon className="navbarSearchIcon" />
                 </div>
+            
+            {/*3 links*/}
+            <div className="navbar_nav">
+                <Link>
+                    <div className="navbar_option">
+                        <span>Hello Georgin</span>
+                        <span>Sign In</span>
+                    </div>
+                </Link>
+            </div>
+
         </nav>
     );
     
