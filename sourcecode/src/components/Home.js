27f3f386a0670e389d773028672dc15956945a80
mr-jestin-roy <roy_jestin@yahoo.com>
2021-08-14 00:47:43 +0530

    navbar op!

--- sourcecode/src/components/Home.js
+++ sourcecode/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import "./Home.css";
 
 export default class home extends Component {
     render(){
         return (
-            <div className="mt-5 d-flex justify-content-left">
-                <h1>WELCOME TO THE HOME PAGE</h1>
+            <div className="home">
+                <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/AmazonDevices/2019/HeroJune21/2x_Echo_Dot_B084DWH53T_3000x1200_new._CB667724833_.jpg" alt="" />
+                
             </div>
         );
 
