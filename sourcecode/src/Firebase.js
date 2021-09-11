14b3ad7fefcff9dc6a98851665c137954fed7e5f
mr-jestin-roy <roy_jestin@yahoo.com>
2021-09-11 15:20:33 +0530

    changes

--- sourcecode/src/Firebase.js
+++ sourcecode/src/Firebase.js
@@ -1,18 +1,16 @@
 import firebase from "firebase";
-
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyAIBTB2LH4yyKsnROa58xiOaa_ZJWNZ79s",
   authDomain: "amaze-mart.firebaseapp.com",
+  databaseURL:
+    "https://amaze-mart-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "amaze-mart",
   storageBucket: "amaze-mart.appspot.com",
   messagingSenderId: "608786231527",
   appId: "1:608786231527:web:fc2dac2446eca7be34b242",
   measurementId: "G-VMR92076DD",
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
+});
 const db = firebaseApp.firestore();
-const auth = firebase.auth();
+const auth = firebaseApp.auth();
 
-export { db, auth };
+export { auth };
--- sourcecode/src/Header.js
+++ sourcecode/src/Header.js
@@ -1,70 +1,71 @@
-
-import React from 'react';
+import React from "react";
 import "./Header.css";
-import {Link} from "react-router-dom";
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { UseStateValue } from "./StateProvider";
 
 function navbar() {
-    const [{basket, user }, dispatch] = UseStateValue();
-    
-    // console.log(basket);
-    return (
-        <nav className="navbar">
-            {/*LOGO*/}
-            <Link to="/">
-            <img className="navbarLogo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
-            </Link>
+  const [{ basket, user }, dispatch] = UseStateValue();
 
-            {/*SEARCH BAR*/}
-                <div className="navbarSearch">
-                  <input type="text" className="navbarSearchInput" />
-                  <SearchIcon className="navbarSearchIcon" />
-                </div>
-            
-            {/*3 links */}
-            <div className="navbar_nav">
-                {/*1st Link */}
-                <Link to="/login" className="navbar_link">
-                    <div className="navbar_option">
-                        <span className="navbar_optionLineOne">Hello Georgin</span>
-                        <span className="navbar_optionLineTwo">Sign In</span>
-                    </div>
-                </Link>
+  // console.log(basket);
+  return (
+    <nav className="navbar">
+      {/*LOGO*/}
+      <Link to="/">
+        <img
+          className="navbarLogo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
 
-                {/*2nd Link */}
-                <Link to="/" className="navbar_link">
-                    <div className="navbar_option">
-                        <span className="navbar_optionLineOne">Returns</span>
-                        <span className="navbar_optionLineTwo">& Orders</span>
-                    </div>
-                </Link>
+      {/*SEARCH BAR*/}
+      <div className="navbarSearch">
+        <input type="text" className="navbarSearchInput" />
+        <SearchIcon className="navbarSearchIcon" />
+      </div>
 
-                {/*3rd Link */}
-                <Link to="/" className="navbar_link">
-                    <div className="navbar_option">
-                        <span className="navbar_optionLineOne">Your</span>
-                        <span className="navbar_optionLineTwo">Prime</span>
-                    </div>
-                </Link>
+      {/*3 links */}
+      <div className="navbar_nav">
+        {/*1st Link */}
+        <Link to="/login" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionLineOne">Hello Georgin</span>
+            <span className="navbar_optionLineTwo">Sign In</span>
+          </div>
+        </Link>
 
-                {/*4th Link*/}
-                <Link to="/checkout">
-                    <div className="navbar_optionBasket">
-                        {/*shopping basket icon*/}
-                        <ShoppingBasketIcon />
-                        {/*Number of items in the basket*/}
-                        <span className="header__optionLineTwo header__basketCount">
+        {/*2nd Link */}
+        <Link to="/" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionLineOne">Returns</span>
+            <span className="navbar_optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        {/*3rd Link */}
+        <Link to="/" className="navbar_link">
+          <div className="navbar_option">
+            <span className="navbar_optionLineOne">Your</span>
+            <span className="navbar_optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        {/*4th Link*/}
+        <Link to="/checkout">
+          <div className="navbar_optionBasket">
+            {/*shopping basket icon*/}
+            <ShoppingBasketIcon />
+            {/*Number of items in the basket*/}
+            <span className="header__optionLineTwo header__basketCount">
               {basket?.length}
             </span>
-                    </div>
-                </Link>
-            </div>
-
-        </nav>
-    );
-    
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default navbar;
